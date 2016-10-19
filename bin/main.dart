3d434dc7ba019fc89c7babba1ad45518d6c768a0
Antonin Lebrard <a.lebrard@gmail.com>
2016-10-19 19:14:59 +0200

    Now change directory tree to [ArtistName]/[AlbumName]/[title]

--- bin/main.dart
+++ bin/main.dart
@@ -6,19 +6,38 @@ main(List<String> args) {
 	showHelp();
 	return;
   }
+  // [ArtistName]-[AlbumName]
   Directory d = new Directory.fromUri(new Uri.directory(args[0], windows: true));
-  d.listSync().forEach((FileSystemEntity f){
-    if (f.path.endsWith(".flac")){
+  if (!d.existsSync()){
+     print("${args[0]} does not exist");
+     return;
+  }
+  String dirName = d.uri.pathSegments[d.uri.pathSegments.length-2];
+  String artistName = dirName.substring(0, dirName.indexOf('-')).replaceAll('_', ' ');
+  String albumName = dirName.substring(dirName.indexOf('-') + 1, dirName.length).replaceAll('_', ' ');
+  String ArtistPath = d.uri.pathSegments.getRange(0, d.uri.pathSegments.length-2).join("\\") + "\\" + artistName + "\\";
+  String AlbumPath = ArtistPath + albumName + "\\";
+  d.renameSync(ArtistPath);
+  Directory album = new Directory.fromUri(new Uri.directory(AlbumPath, windows: true));
+  album.createSync();
+  List<FileSystemEntity> filesToMove = new List();
+  d = new Directory.fromUri(new Uri.directory(ArtistPath, windows: true));
+  d.listSync().forEach((FileSystemEntity f) {
+    if (FileSystemEntity.isFileSync(f.path)) {
       String filename = "" + f.uri.pathSegments.last;
-      // 0[noDisc]-[noSong][noSong]-[Artist]-[SongName]-LLS.flac
-      print("renaming $filename");
-      // [noSong][noSong]-[Artist]-[SongName]
-      filename = filename.substring(3, filename.length-("-LLS.flac".length));
-      // [noSong][noSong]-[SongName]
-      filename = filename.substring(0, 3) + filename.substring(filename.indexOf("-", 4)+1, filename.length);
-      filename = filename.replaceAll("_", " ");
-      print("to $filename");
-      f.renameSync(f.uri.pathSegments.getRange(0, f.uri.pathSegments.length-1).join("\\") + "\\$filename" + ".flac");
+      if (f.path.endsWith(".flac")) {
+        // 0[noDisc]-[noSong][noSong]-[Artist]-[SongName]-LLS.flac
+        print("renaming $filename");
+        // [noSong][noSong]-[Artist]-[SongName]
+        filename = filename.substring(3, filename.length - ("-LLS.flac".length));
+        // [noSong][noSong]-[SongName]
+        filename = filename.substring(0, 3) + filename.substring(filename.indexOf("-", 4) + 1, filename.length);
+        filename = filename.replaceAll("_", " ");
+        filename += ".flac";
+        print("to $filename");
+      }
+      f.renameSync(f.uri.pathSegments.getRange(0, f.uri.pathSegments.length - 1).join("\\") + "\\$albumName\\$filename");
+      filesToMove.add(f);
     }
   });
 }
